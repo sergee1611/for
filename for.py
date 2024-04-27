63bafcfeff0fac259accd4cefd9c3f708012c35c
sergee1611 <167037492+sergee1611@users.noreply.github.com>
2024-04-27 21:52:34 +0300

    Add files via upload

---
+++ for.py
@@ -0,0 +1,6 @@
+list_of_cars = ['BMW', 'MB', 'LADA', 'KIA', 'HONDA']
+cars_count = 0
+for i in range(len(list_of_cars)):
+    print('Я езжу на автомобиле марки', list_of_cars[i])
+    cars_count += 10
+    print(cars_count)
